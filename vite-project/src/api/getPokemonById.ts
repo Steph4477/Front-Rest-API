b0196695f463d8dc3097926e7f58436e76f4243a
Eteib Dembele <dembeleeteib@gmail.com>
2024-01-03 12:35:23 +0100

    refactore code

---
+++ vite-project/src/api/getPokemonById.ts
@@ -0,0 +1,24 @@
+export interface Pokemon {
+  name: string;
+  type: string;
+  attack: number;
+  defense: number;
+  description: string;
+  image: string;
+}
+
+export async function getPokemonById(id: number): Promise<Pokemon> {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const data = await response.json();
+
+  const pokemon: Pokemon = {
+    name: data.name,
+    type: data.types[0].type.name,
+    attack: data.stats[1].base_stat,
+    defense: data.stats[2].base_stat,
+    description: data.species.name,
+    image: data.sprites.front_default,
+  };
+
+  return pokemon;
+}
--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,50 +1,23 @@
-import './style.css'
-//import typescriptLogo from './typescript.svg'
-//import viteLogo from '/vite.svg'
-//import { setupCounter } from './counter.ts'
+import { getPokemonById } from './api/getPokemonById';
 
-/*document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)*/
 const appElement = document.getElementById('app');
 
 if (appElement) {
   const ulElement = document.createElement('ul');
   appElement.appendChild(ulElement);
 
-  async function fetchPokemonList() {
-    try {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon');
-      const data = await response.json();
+  async function displayPokemon(id: number) {
+    const pokemon = await getPokemonById(id);
 
-      const pokemonList = data.results;
+    const liElement = document.createElement('li');
+    liElement.textContent = `Nom: ${pokemon.name}, Type: ${pokemon.type}, Attaque: ${pokemon.attack}, Défense: ${pokemon.defense}, Description: ${pokemon.description}`;
 
-      pokemonList.forEach((pokemon: any) => {
-        const liElement = document.createElement('li');
-        liElement.textContent = pokemon.name;
-        ulElement.appendChild(liElement);
-      });
-    } catch (error) {
-      console.error('Erreur de connection ', error);
-    }
+    const imgElement = document.createElement('img');
+    imgElement.src = pokemon.image;
+    liElement.appendChild(imgElement);
+
+    ulElement.appendChild(liElement);
   }
 
-  fetchPokemonList();
+  displayPokemon(2);
 }
-

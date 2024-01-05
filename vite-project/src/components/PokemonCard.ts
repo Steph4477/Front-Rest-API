7e6492df20d9c15b99c0b42e90ddcf9ece3c6379
Steph4477 <morelstephane4026@neuf.fr>
2024-01-05 20:37:56 +0100

    AncreIdAllpokemons

--- vite-project/src/components/PokemonCard.ts
+++ vite-project/src/components/PokemonCard.ts
@@ -20,18 +20,24 @@ export async function createPokemonCard(pokemon: any) {
     const card = document.createElement('article');
     card.className = 'pokemon-card';
 
+    const link = document.createElement('a');
+    link.href = `http://localhost:5173/pokemon/${pokemonDetail.id}`;
+    card.appendChild(link);
+
     const name = document.createElement('h2');
     name.textContent = capitalizeFirstLetter(pokemonDetail.name);
-    card.appendChild(name);
+    link.appendChild(name);
 
     const id = document.createElement('span');
     id.textContent = `ID: ${pokemonDetail.id}`;
-    card.appendChild(id);
+    link.appendChild(id);
 
     const image = document.createElement('img');
     image.src = pokemonDetail.sprites.front_default;
     image.alt = 'Image du Pokémon ' + capitalizeFirstLetter(pokemonDetail.name);
-    card.appendChild(image);
+    link.appendChild(image);
+
+    // ... Reste du code ...
 
     return card;
 }

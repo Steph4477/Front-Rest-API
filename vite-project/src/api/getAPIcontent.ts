45236f44ab7e9f42c9a89622a70b517045045a0e
Steph4477 <morelstephane4026@neuf.fr>
2024-01-05 19:48:34 +0100

    use utils.ts for fetch to API

--- vite-project/src/api/getAPIcontent.ts
+++ vite-project/src/api/getAPIcontent.ts
@@ -1,8 +1,9 @@
+import { fetchData } from './utils';
+
 export async function getAPIContent() {
     try {
-        const apiData = await fetch("https://pokeapi.co/api/v2/")
-            .then(res => res.json());
-            console.log(apiData);
+        const apiData = await fetchData("https://pokeapi.co/api/v2/");
+        console.log(apiData);
     } catch (error) {
         console.error("Error fetching data:", error);
     }

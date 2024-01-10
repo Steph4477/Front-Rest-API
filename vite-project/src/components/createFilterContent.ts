c7b5837d202170dded8b7c653199b2cdb2458b19
unknown <emma.broudic35@gmail.com>
2024-01-10 16:44:29 +0100

    code filtres refactoré

--- vite-project/src/components/createFilterContent.ts
+++ vite-project/src/components/createFilterContent.ts
@@ -1,36 +1,40 @@
-/*import { getPokemonsByFilter} from '../api/getPokemonsByFilter.ts';
+import { getPokemonsByFilter} from '../api/getPokemonsByFilter.ts';
 import { displayPage } from './displayPage.ts';
 import { createPagination } from '../components/Pagination';
 
-export async function createFilterContent(filter: string, filterElement: string, clickCount: number) {
+export async function createFilterContent(selectedFilters: string[], filter: string, filterElement: string, clickCount: number) {
     
     try {
-
-        const pokemonList = await getPokemonsByFilter(filter, filterElement);
-
         const cartDom = document.querySelector<HTMLDivElement>('.pokemonBloc');
         const cartDomFilter = document.querySelector<HTMLDivElement>('.pokemonBlocFilter');
         const paginationDom = document.querySelector<HTMLDivElement>('#pagination-bloc');
 
-        if (cartDom && cartDomFilter && paginationDom) {
-/*
-            if (clickCount % 2 !== 0) {
-
-                cartDom.style.display = 'none';
-                cartDomFilter.style.display = 'flex';
-
-                displayPage(1, cartDomFilter, pokemonList);
-
-                const paginationContainer = createPagination(pokemonList, 1, cartDomFilter);
-                paginationDom.appendChild(paginationContainer);
-
-            } else {
-
-                cartDom.style.display = 'flex';
-                cartDomFilter.style.display = 'none';
-            }
+        if (clickCount % 2 !== 0) {
+            selectedFilters.push(filterElement);
+        } else {
+            selectedFilters = selectedFilters.filter(item => item !== filterElement);
         }
+
+        console.log(selectedFilters);
+        const pokemonList = await getPokemonsByFilter(filter, selectedFilters);
+
+        if (selectedFilters.length > 0 && cartDom && cartDomFilter && paginationDom) {
+            cartDom.style.display = 'none';
+            cartDomFilter.style.display = 'flex';
+
+            displayPage(1, cartDomFilter, pokemonList);
+
+            const paginationContainer = createPagination(pokemonList, 1, cartDomFilter);
+            paginationDom.appendChild(paginationContainer);
+
+        } else if (selectedFilters.length === 0 && cartDom && cartDomFilter && paginationDom){
+            cartDom.style.display = 'flex';
+            cartDomFilter.style.display = 'none';
+
+            displayPage(1, cartDomFilter, pokemonList);
+        }
+
     } catch (error){
         console.log(error);
     }
-}*/
+}

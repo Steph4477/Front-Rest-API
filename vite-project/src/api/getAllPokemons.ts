2a4cf7f4c7924092f08d151443c0741badf9687f
Steph4477 <morelstephane4026@neuf.fr>
2024-01-12 10:48:58 +0100

    perform

--- vite-project/src/api/getAllPokemons.ts
+++ vite-project/src/api/getAllPokemons.ts
@@ -1,14 +1,23 @@
 // getAllPokemons.ts
 import { fetchData } from './utils.ts';
+import { LRUCacheOnMap } from './cache';
+
+const cache = new LRUCacheOnMap(100); // Capacité de 100 éléments
 
 export async function getAllPokemons() {
-  let nextUrl: string = 'https://pokeapi.co/api/v2/pokemon/?limit=20000';
-  const pokemons: any[] = [];
+  let pokemons = cache.get('pokemons');
 
-  while (nextUrl) {
-    const pokemonsResponse: any = await fetchData(nextUrl);
-    pokemons.push(...pokemonsResponse.results);
-    nextUrl = pokemonsResponse.next;
+  if (!pokemons) {
+    let nextUrl: string = 'https://pokeapi.co/api/v2/pokemon/?limit=20000';
+    pokemons = [];
+
+    while (nextUrl) {
+      const pokemonsResponse: any = await fetchData(nextUrl);
+      pokemons.push(...pokemonsResponse.results);
+      nextUrl = pokemonsResponse.next;
+    }
+
+    cache.set('pokemons', pokemons);
   }
 
   return pokemons;
--- vite-project/src/components/displayPage.ts
+++ vite-project/src/components/displayPage.ts
@@ -7,12 +7,6 @@ import { createPokemonCard } from './PokemonCard';
 // Nombre de Pokémon affichés par page
 const pageSize = 30;
 
-/**
- * Affiche une page spécifiée de cartes Pokémon dans le conteneur fourni.
- * @param {number} page - Le numéro de la page à afficher.
- * @param {HTMLElement} container - L'élément HTML dans lequel afficher les cartes Pokémon.
- * @param {any[]} pokemons - La liste complète des Pokémon à paginer.
- */
 export async function displayPage(page: number, container: HTMLElement, pokemons: Pokemon[]) {
     // Calcule l'indice de départ et l'indice de fin pour la pagination
     const startIndex = (page - 1) * pageSize;
@@ -25,18 +19,20 @@ export async function displayPage(page: number, container: HTMLElement, pokemons
     container.innerHTML = '';
 
     // Boucle à travers les Pokémon de la page et crée une carte pour chacun
-    for (const pokemon of pokemonsOnPage) {
+    
         try {
-            // Crée une carte Pokémon en attendant la résolution de la promesse
-            const card = await createPokemonCard(pokemon);
-
-            // Vérifie si la carte a été créée avec succès et l'ajoute au conteneur
-            if (card) {
-                container.appendChild(card);
-            }
+            const cards = await Promise.all(pokemonsOnPage.map((pokemon) => createPokemonCard(pokemon)));
+            cards.forEach((card) => {
+                if (card) {
+                    container.appendChild(card);
+                }
+            });
         } catch (error) {
             // Gère les erreurs lors de la création de la carte pour un Pokémon spécifique
             console.error(`Failed to create card for pokemon: ${pokemons}`, error);
         }
     }
-}
+
+        
+    
+
